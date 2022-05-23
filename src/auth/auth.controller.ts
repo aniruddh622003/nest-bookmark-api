6d29987f72ff8ad7085ce67518ad24e92ed1ffb7
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-05-24 01:48:40 +0530

    Progress till 23-05-22

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth') //Global auth end point
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup') //Local end point, eg this is /auth/signup
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+}
